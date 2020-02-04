2f367f57bf90cf68844972f9b3df9e0a4dc6b6c7
Nicolas Conti <n.conti@kayrros.com>
2020-02-05 00:59:24 +0100

    handle commands

---
+++ script.js
@@ -0,0 +1,19 @@
+function stdin(input) {
+    // If enter is pressed:
+    if (event.keyCode == 13) {
+        // create placeholder from duplicated node:
+        var parent = input.parentNode; // <pre>
+        var child = parent.cloneNode(true);
+        // write output from input command:
+        var output = child.getElementsByTagName('output')[0]; // <output>
+        output.value = exec(input.value);
+        // clear current node:
+        input.value = "";
+        // insert placeholder before node:
+        parent.parentNode.insertBefore(child, parent);
+    }
+}
+
+function exec(command) {
+    return "bash: command not found: ".concat(command);
+}
