c99179e05f204d671150bc23664d19537ee4e07d
Nicolas Conti <n.conti@kayrros.com>
2020-02-05 23:05:26 +0100

    handle clear command

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 function stdin(input) {
     // If enter is pressed:
     if (event.keyCode == 13) {
+        var command = input.value
         // create placeholder from duplicated node:
         var parent = input.parentNode; // <pre>
         var clone = parent.cloneNode(true);
         var placeholder = clone.getElementsByTagName('output')[0]; // <output>
         // write output from input command:
-        placeholder.value = exec(input.value);
+        placeholder.value = exec(command);
         // clear current prompt:
         input.value = "";
         // insert placeholder before prompt:
         parent.parentNode.insertBefore(clone, parent);
+        // Handle cleaning:
+        if (command == "clear") {
+            var terminal = parent.parentNode
+            while (terminal.childElementCount > 1) {
+                terminal.removeChild(terminal.firstChild);
+            }
+        }
     }
 }
 
 function exec(command) {
     switch (command) {
         case "":
+            return ""
+        case "clear":
+            return ""
+        case "bash":
+            return ""
+        case "/bin/bash":
             return ""
         default:
             return "bash: command not found: ".concat(command);
