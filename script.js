79df099ab6ac0a722d372073a4a3903ad6cf9694
Nicolas Conti <n.conti@kayrros.com>
2020-02-08 21:52:39 +0100

    code cleaning

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ function stdin(input) {
     // If enter is pressed:
     if (event.keyCode == 13) {
         var command = input.value;
+
         // create placeholder from duplicated node:
         var parent = input.parentNode.parentNode; // <pre>
         var clone = parent.cloneNode(true);
+
         // write output from input command:
         var output = exec(command);
         clone.appendChild(output)
+
         // clear current prompt:
         input.value = "";
+
         // insert placeholder before prompt:
         parent.parentNode.insertBefore(clone, parent);
+
         // Handle cleaning:
         if (command == "clear") {
             var terminal = parent.parentNode;
@@ -24,31 +29,38 @@ function stdin(input) {
 
 function exec(command) {
     switch (command) {
+
         case "":
         case "clear":
         case "bash":
         case "/bin/bash":
-            var out = document.getElementById("text-output").cloneNode(true);
-            out.style.display = "flex";
-            return out;
+            return getElement("text-output")
+
         case "output0":
-            var out = document.getElementById(command).cloneNode(true);
-            out.style.display = "flex";
-            return out;
+            return getElement(command)
+
         default:
-            var out = document.getElementById("text-output").cloneNode(true);
-            out.style.display = "flex";
+            var out = getElement("text-output")
+
             // if command contains /
             if (command.indexOf("/") !== -1) {
                 out.value = "bash: " + command + ": No such file or directory";
                 return out;
             }
+
             out.value = "bash: command not found: ".concat(command);
             return out;
     }
 }
 
-function promptFocus(){
+function getElement(id) {
+    var out = document.getElementById(id).cloneNode(true);
+    out.removeAttribute("id")
+    out.style.display = "flex";
+    return out
+}
+
+function promptFocus() {
     var inputs = document.getElementsByTagName('input');
-    inputs[inputs.length-1].focus();
+    inputs[inputs.length - 1].focus();
 }
